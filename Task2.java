7d797bc76a372a5c8713335d7004bc6bb189d760
jayasritagarampudi <41276852+jayasritagarampudi@users.noreply.github.com>
2018-07-18 16:19:12 +0530

    Task2

---
+++ Task2.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class Task2{
+public static void main(String[] args){
+Pattern compile=Pattern.compile("yahoo[.][a-z]{3}[.][a-z]{2}");
+Matcher matcher=compile.matcher("yahoo.com.co");
+while(matcher.find())
+{
+System.out.println(matcher.group());
+}
+}
+}
